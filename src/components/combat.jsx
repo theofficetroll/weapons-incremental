8b1da0409c65e57bc94a65ef8e3b2775b66a9491
Mark <mark.scrudder@gmail.com>
2021-02-20 06:59:02 -0800

    Built progress bar template with a random value being sent to it every second by Combat component to test.

--- src/components/combat.jsx
+++ src/components/combat.jsx
@@ -1,24 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ProgressBar from './progressBar.jsx';
 
 // This handles the combat screen for the game
 // Will show wielder and opponent
 // This will contain all timing?
 
-class Combat extends React.Component {
-  constructor (props) {
-    super (props);
+function Combat() {
+  const [completed, setCompleted] = useState(0);
+
+  const combatStyles = {
+    textAlign: 'center',
+    width: '50%',
   }
 
-  render () {
-    return (
-      <div className="combat">
-        Combat Panel
-        <br />
-        <ProgressBar bgcolor='#6a1b9a' completed='10' />
-      </div>
-    )
-  }
+  useEffect(() => {
+    setInterval(() => setCompleted(Math.floor(Math.random() * 100) + 1), 1000);
+  }, []);
+
+  return (
+    <div style={combatStyles} className="Combat">
+      <ProgressBar bgcolor={"#6a1b9a"} completed={completed} />
+    </div>
+  );
 }
+
+
+// const Combat = props => {
+//   // const { wielder, opponent, weapon } = props;
+
+//   const [ wielderProgress, opponentProgress ] = useState(0);
+
+
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       updateValue(oldValue => {
+//         const newValue = oldValue + 10;
+
+//         if (newValue === 100) {
+//           // Trigger attack
+//           // Then reset interval
+//           clearInterval(interval);
+//         }
+//         return newValue;
+//       });
+//     }, 1000);
+//   }, []);
+
+//   return (
+//     <div className="combat">
+//       Combat Panel
+//       <br />
+//       Wielder Progress Bar:
+//       <ProgressBar bgcolor='#6a1b9a' completed={completed} />
+//     </div>
+//   )
+// }
 
 export default Combat;
--- src/components/progressBar.jsx
+++ src/components/progressBar.jsx
@@ -2,8 +2,37 @@ import React from 'react';
 
 const ProgressBar = (props) => {
   const {bgcolor, completed } = props;
+
+  const containerStyles = {
+    height: 20,
+    width: '100%',
+    backgroundColor: "#e0e0de",
+    borderRadius: 50,
+    margin: 50,
+  }
+
+  const fillerStyles = {
+    height: '100%',
+    width: `${completed}%`,
+    backgroundColor: bgcolor,
+    transition: 'width 1s ease-in-out',
+    borderRadius: 'inherit',
+    textAlign: 'right',
+  }
+
+  const labelStyles = {
+    padding: 5,
+    color: 'white',
+    fontWeight: 'bold',
+  }
+
   return (
-    <progress className='progress-bar' value={completed} max='100' />
+    // <progress className='progress-bar' value={completed} max='100' />
+    <div style={containerStyles}>
+      <div style={fillerStyles}>
+        <span style={labelStyles}>{`${completed}%`}</span>
+      </div>
+    </div>
   )
 }
 
