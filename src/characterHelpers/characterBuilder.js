361fe1eeda5815fe5f03be60d8b4e1b0ab4fd742
Mark <mark.scrudder@gmail.com>
2021-03-16 17:26:32 -0700

    Added stubs for characterBuilder and characterLeveler

---
+++ src/characterHelpers/characterBuilder.js
@@ -0,0 +1,2 @@
+// This will hold main & helper functions to generate new characters
+// This will function the same for wielders and opponents
---
+++ src/characterHelpers/characterLeveler.js
@@ -0,0 +1,1 @@
+// This will handle the leveling up of a character when reach next xp tier
